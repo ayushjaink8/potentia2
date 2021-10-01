4cce3a17d65647af42be3adb1d9a2b64bf7d2373
Ayush Jain <ayushjaink8@gmail.com>
2021-10-01 23:22:09 +0530

    removed comments in helpers.js]

--- keys/helpers.js
+++ keys/helpers.js
@@ -7,46 +7,10 @@ const rMap = {
     'grandfather':2,'grandmother':2,
     'great-grandfather':3,'great-grandmother':3
 }
-// const siblingMap = {
-//     'great-grandson':0, 'great-granddaughter':0,
-//     'grandson': 0,'granddaughter': 0,
-//     'son':0,'daughter':0,'nephew':1,'niece':1,
-//     'wife':1,'husband':1,'brother':1,'sister':1,'cousin':1,'brother-in-law':2,'sister-in-law':2,'fiance':1,
-//     'father':0,'mother':0,'father-in-law':1,'mother-in-law':1,'uncle':1,'aunt':1,'maternal-uncle':1,'paternal-uncle':1,'maternal-aunt':1,'paternal-aunt':1,
-//     'grandfather':0,'grandmother':0,
-//     'great-grandfather':0,'great-grandmother':0
-// }
-// const oppositeMap = {
-//     'great-grandson':'great-grandparent', 'great-granddaughter':'great-grandparent',
-//     'grandson': 'grandparent','granddaughter': 'grandparent',
-//     'son':'parent','daughter':'parent','nephew':'uncle/aunt','niece':'uncle/aunt',
-//     'wife':'husband','husband':'wife','brother':'sibling','sister':'sibling','cousin':'cousin','brother-in-law':'sibling-in-law','sister-in-law':'sibling-in-law','fiance':'fiance',
-//     'father':'child','mother':'child','father-in-law':'child-in-law','mother-in-law':'child-in-law','uncle':'nibling','aunt':'nibling','maternal-uncle':'nibling','paternal-uncle':'nibling','maternal-aunt':'nibling','paternal-aunt':'nibling',
-//     'grandfather':'grandchild','grandmother':'grandchild',
-//     'great-grandfather':'great-grandson','great-grandmother':'great-grandson'
-// }
 
 const csvHeaders = [
     'PID', 'Primary Person', 'SID', 'Secondary Person', 'Relationship'
 ]
-
-// function csvToArray(str, delimiter = ",") {
-//     const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-//     for(let i = 0; i < 5; i++) {
-//         headers[i] = headers[i].trim();
-//         if(headers[i] != csvHeaders[i]) return [];
-//     }
-//     const rows = str.slice(str.indexOf("\n") + 1).split("\n");
-//     const result = rows.map(function (row) {
-//         const values = row.split(delimiter);
-//         const el = headers.reduce(function (object, header, index) {
-//             object[header] = values[index]?.trim();
-//             return object;
-//         }, {});
-//       return el;
-//     });
-//     return result;
-// }
 
 function CreateDataObjects(data){
     const result = [];
@@ -98,10 +62,7 @@ function parseDate(date){
 }
 module.exports = {
     rMap,
-    // oppositeMap,
-    // siblingMap,
     csvHeaders,
-    // csvToArray,
     CreateDataObjects,
     toTitleCase,
     parseNumber,
