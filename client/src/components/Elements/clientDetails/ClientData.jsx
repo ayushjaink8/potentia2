d34f5b15cd9ee3c2a67a265680747b983902d8f9
Ayush Jain <ayushjaink8@gmail.com>
2021-10-02 18:00:28 +0530

    removed icom from client Data

--- client/src/components/Elements/clientDetails/ClientData.jsx
+++ client/src/components/Elements/clientDetails/ClientData.jsx
@@ -3,12 +3,10 @@ import {
   Grid,
   Box
 } from '@material-ui/core';
-import Info_icon from '../info_icon.svg'
 const ClientData = () => {
   return (
     <>
       <Box m={3} textAlign="center">
-        <img src={Info_icon} style={{height:'20px'}}/>
         "Selected Client Data here"  // notes and all other data if it is required
       </Box>
     </>
